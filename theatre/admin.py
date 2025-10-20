569f3b3023489699db1f20c322663502419b4c60
Vik <viktoria.om@gmail.com>
2025-10-20 17:38:42 +0300

    add new app theatre, create Actor, Genre, Play, Review, ThetreHall, Performance, Reservation, Ticket models & register all models in theatre/admin

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import (
+    Performance,
+    Review,
+    Ticket,
+    TheatreHall,
+    Reservation,
+    Play,
+    Actor,
+    Genre
+)
+
+
+admin.site.register(TheatreHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(Performance)
+admin.site.register(Reservation)
+admin.site.register(Review)
+admin.site.register(Ticket)
--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,172 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Avg
 
-# Create your models here.
+from theatre_api import settings
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres  = models.ManyToManyField(
+        Genre,
+        blank=True,
+        related_name="plays"
+    )
+    actors = models.ManyToManyField(
+        Actor,
+        blank=True,
+        related_name="plays"
+    )
+
+    @property
+    def rating(self):
+        return self.reviews.aggregate(avg_rating=Avg("rating"))["avg_rating"]
+
+
+    class Meta:
+        verbose_name_plural = "Plays"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    comment = models.TextField()
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    def __str__(self):
+        return self.comment
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    theatre_hall = models.ForeignKey(
+        TheatreHall,
+        on_delete=models.CASCADE,
+        related_name="performances"
+    )
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return self.play.title + " " + str(self.show_time)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(
+        Performance,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    class Meta:
+        ordering = ["row", "seat"]
+        unique_together = ("row", "seat", "reservation")
+
+    def __str__(self):
+        return f"({str(self.performance)}: row {self.row}, seat {self.seat}"
+
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        *args,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
