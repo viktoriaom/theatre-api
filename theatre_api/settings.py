c86a9d9ccb00018dae802e924c94c3c4b468e65a
Vik <viktoria.om@gmail.com>
2025-10-20 23:35:58 +0300

    add JWTAuthentication & ManageUserView

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=123456789)
+}
--- user/views.py
+++ user/views.py
@@ -9,10 +9,10 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-# class ManageUserView(generics.RetrieveUpdateAPIView):
-#     serializer_class = UserSerializer
-#     authentication_classes = (JWTAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def get_object(self):
-#         return self.request.user
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
