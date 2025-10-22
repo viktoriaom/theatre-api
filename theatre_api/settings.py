7f9bbbc2aa71b8517007efe4085319d43530cdc4
Vik <viktoria.om@gmail.com>
2025-10-22 23:39:22 +0300

    add DEFAULT_THROTTLE_CLASSES & DEFAULT_THROTTLE_RATES to settings.py

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -135,7 +135,15 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10
+    "PAGE_SIZE": 10,
+    "DEFAULT_THROTTLE_CLASSES": [
+       "rest_framework.throttling.AnonRateThrottle",
+       "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+       "anon": "100/day",
+       "user": "1000/day"
+    }
 }
 
 SIMPLE_JWT = {
