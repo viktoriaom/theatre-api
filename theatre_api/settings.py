92f26fcbc3d76ae6b8077dd0a3474c16f0c0b2aa
Vik <viktoria.om@gmail.com>
2025-11-03 23:30:11 +0200

    fix indent in DEFAULT_PAGINATION_CLASS

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS":
         "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,
+        "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
