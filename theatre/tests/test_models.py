677c8334a3fb21c6a8a580562b96b65206df3529
Vik <viktoria.om@gmail.com>
2025-11-10 23:49:33 +0200

    update models tests with helpers functions

--- theatre/tests/test_models.py
+++ theatre/tests/test_models.py
@@ -1,6 +1,4 @@
 import math
-from datetime import datetime
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.core.exceptions import ValidationError
@@ -8,55 +6,20 @@ from django.core.exceptions import ValidationError
 from theatre.models import (
     Actor,
     Genre,
-    Play,
     Review,
-    TheatreHall,
-    Performance,
-    Reservation,
     Ticket,
+)
+
+from theatre.tests.helpers import (
+    create_and_login_user,
+    create_test_play,
+    create_test_theatre_hall,
+    create_test_performance,
+    create_test_reservation
 )
 
 
 class ModelsTests(TestCase):
-
-    def create_user(self):
-        user = get_user_model().objects.create_user(
-            email="<EMAIL>", password="<PASSWORD>"
-        )
-        return user
-
-    def create_play(self):
-        play = Play.objects.create(
-            title="Test_title",
-            description="Test_description",
-        )
-        return play
-
-    def create_theatre_hall(self):
-        theatre_hall = TheatreHall.objects.create(
-            name="Test_theatre_hall",
-            rows=10,
-            seats_in_row=20,
-        )
-        return theatre_hall
-
-    def create_performance(self):
-        play = self.create_play()
-        theatre_hall = self.create_theatre_hall()
-        performance = Performance.objects.create(
-            play=play,
-            theatre_hall=theatre_hall,
-            show_time=datetime.now()
-        )
-        return performance
-
-    def create_reservation(self):
-        user = self.create_user()
-        reservation = Reservation.objects.create(
-            created_at=datetime.now(),
-            user=user
-        )
-        return reservation
 
     def test_actor_str_and_full_name(self):
         actor = Actor.objects.create(
@@ -72,11 +35,11 @@ class ModelsTests(TestCase):
         self.assertEqual(str(genre), genre.name)
 
     def test_play_str(self):
-        play = self.create_play()
+        play = create_test_play()
         self.assertEqual(str(play), play.title)
 
     def test_theatre_hall_str_and_capacity(self):
-        theatre_hall = self.create_theatre_hall()
+        theatre_hall = create_test_theatre_hall()
         self.assertEqual(str(theatre_hall), theatre_hall.name)
         self.assertEqual(
             theatre_hall.capacity,
@@ -84,23 +47,23 @@ class ModelsTests(TestCase):
         )
 
     def test_performance_str(self):
-        performance = self.create_performance()
+        performance = create_test_performance()
         self.assertEqual(
             str(performance),
             performance.play.title + " " + str(performance.show_time)
         )
 
     def test_review_str(self):
-        play = self.create_play()
-        user = self.create_user()
+        play = create_test_play()
+        user, _ = create_and_login_user()
         review = Review.objects.create(
             user=user, play=play, rating=5, comment="Test_comment"
         )
         self.assertEqual(str(review), review.comment)
 
     def test_play_rating(self):
-        play = self.create_play()
-        first_user = self.create_user()
+        play = create_test_play()
+        first_user, _ = create_and_login_user()
         self.assertEqual(play.rating, None)
         first_review = Review.objects.create(
             user=first_user, play=play, rating=5, comment="Test_comment"
@@ -118,12 +81,12 @@ class ModelsTests(TestCase):
         )
 
     def test_reservation_str(self):
-        reservation = self.create_reservation()
+        reservation = create_test_reservation()
         self.assertEqual(str(reservation), str(reservation.created_at))
 
     def test_ticket_str(self):
-        reservation = self.create_reservation()
-        performance = self.create_performance()
+        reservation = create_test_reservation()
+        performance = create_test_performance()
         ticket = Ticket.objects.create(
             performance=performance,
             reservation=reservation,
@@ -137,8 +100,8 @@ class ModelsTests(TestCase):
         )
 
     def test_ticket_validation(self):
-        reservation = self.create_reservation()
-        performance = self.create_performance()
+        reservation = create_test_reservation()
+        performance = create_test_performance()
 
         ticket = Ticket(
             performance=performance,
